09ab85ace73678b7700041248153022a5b51c752
Codeirie_YT <malacprom@gmail.com>
2025-12-23 20:14:29 -0600

    Fixed Installer.

--- Installer.py
+++ Installer.py
@@ -14,6 +14,9 @@ match os.name:
     case 'posix':
         pass
     case 'nt':
-        os.system(f'mkdir "C:\\SYNULBBAT" & echo \'@echo off \npy "{get_script_dir()}\\ProgramFiles\\main.py" %*\' > "C:\\SYNULBBAT\\synulb.bat" & powershell -Command "[System.Environment]::SetEnvironmentVariable(\'Path\', $env:Path + \';C:\SYNULBBAT\', [System.EnvironmentVariableTarget]::User)"')
+        os.system(f'mkdir "C:\\SYNULBBAT"')
+        os.system(f'(echo @echo off) > "C:\\SYNULBBAT\\synulb.bat"')
+        os.system(f'(echo py "{get_script_dir()}\\ProgramFiles\\main.py" %*) >> "C:\\SYNULBBAT\\synulb.bat"')
+        os.system(f'powershell -Command "[System.Environment]::SetEnvironmentVariable(\'Path\', $env:Path + \';C:\SYNULBBAT\', [System.EnvironmentVariableTarget]::User)"')
     case 'java':
         print("Sorry, but JPython is not a supported interperter for Synulb. You will have to run ProgramFiles-main.py manually.")
